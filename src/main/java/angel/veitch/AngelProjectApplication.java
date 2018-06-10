0a422a42e934d83116c4a1d24add9b54ff36115e
veitch <weiqi.wang02@ele.me>
2018-06-11 00:31:56 +0800

    init

---
+++ src/main/java/angel/veitch/AngelProjectApplication.java
@@ -0,0 +1,12 @@
+package angel.veitch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngelProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngelProjectApplication.class, args);
+	}
+}
---
+++ src/test/java/angel/veitch/AngelProjectApplicationTests.java
@@ -0,0 +1,16 @@
+package angel.veitch;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AngelProjectApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
